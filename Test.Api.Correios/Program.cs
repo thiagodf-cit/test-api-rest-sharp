8a4448692d3c8fc5c9c18b58bbf16b38891b9519
martins86 <martins86@gmail.com>
2020-07-19 23:23:39 +0000

    test exception

--- Test.Api.Correios/Program.cs
+++ Test.Api.Correios/Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
 using Test.Api.Correios.Models;
 using RestSharp;
 using Xunit;
 using FluentAssertions;
 using Newtonsoft.Json;
+using Test.Api.Correios.Utils;
 
 namespace Test.Api.Correios
 {
     public class Program
     {
+        private ReadCepDb readCepDb = ReadCepDb.ReadCep();
         private IRestResponse GetCep(object cep)
         {
             var rsClient = new RestClient("http://viacep.com.br/ws/" + cep + "/json/");
@@ -22,10 +24,10 @@ namespace Test.Api.Correios
         public void Sucesso()
         {
             //Arrange
-            var cep = "01138000";
+            var cepValid = readCepDb.CepValid;
 
             //Act
-            var response = GetCep(cep);
+            var response = GetCep(cepValid);
             var json = JsonConvert.DeserializeObject<CorreiosResponse>(response.Content);
 
             //Assert
@@ -39,6 +41,30 @@ namespace Test.Api.Correios
             json.Ibge.Should().Be("3550308");
             json.Gia.Should().Be("1004");
 
+            response.StatusCode.Should().Be(200);
+        }
+        
+        [Fact]
+        public void Exception()
+        {
+             //Arrange
+            var cepInvalid = readCepDb.CepInvalid;
+
+            //Act
+            var response = GetCep(cepInvalid);
+            var json = JsonConvert.DeserializeObject<CorreiosResponse>(response.Content);
+
+            //Assert
+            json.Cep.Should().BeNull();
+            json.Logradouro.Should().BeNull();
+            json.Complemento.Should().BeNull();
+            json.Bairro.Should().BeNull();
+            json.Localidade.Should().BeNull();
+            json.UF.Should().BeNull();
+            json.Unidade.Should().BeNull();
+            json.Ibge.Should().BeNull();
+            json.Gia.Should().BeNull();
+            
             response.StatusCode.Should().Be(200);
         }
     }
